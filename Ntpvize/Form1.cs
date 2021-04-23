822280fd8b2f82c7e3bd7c6c970b202e1a8de676
volkansoytorun <volkansoytorun@gmail.com>
2021-04-23 16:57:58 +0300

    güncel veriler alındı

--- Ntpvize/Form1.cs
+++ Ntpvize/Form1.cs
@@ -9,6 +9,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Timers;
 using System.Xml;
+using System.Threading;
+
 namespace Ntpvize
 {
     public partial class Form1 : Form
@@ -17,34 +19,58 @@ namespace Ntpvize
         {
             InitializeComponent();
         }
-   
 
-        private void button1_Click(object sender, EventArgs e)
+
+
+        int enguncelveriid = 0;
+        void verileriyaz()
         {
-           
+          
             XmlTextReader xmloku = new XmlTextReader("https://www.webtekno.com/rss.xml");
+            string veriler = "";
+            bool item = false;
+            int i = 0;
             
             while (xmloku.Read())
             {
-               
-
+                if (xmloku.Name == "item")
+                {
+                    item = true;
+                }
+                if (item == false)
+                {
+                    continue;
+                }
+                i++;
                 if (xmloku.Name == "id")
                 {
-                    listBox1.Items.Add(xmloku.ReadString());
+                    if (i == 3)
+                    {
+                        enguncelveriid = Convert.ToInt32(xmloku.ReadString());
+                    }
+
+                    veriler += xmloku.ReadString() + Environment.NewLine;
                 }
                 if (xmloku.Name == "title")
-                {
-                    listBox1.Items.Add(xmloku.ReadString());
+                { 
+
+                   
+                    veriler += xmloku.ReadString() + Environment.NewLine;
                 }
                 if (xmloku.Name == "description")
                 {
-                    listBox1.Items.Add(xmloku.ReadString());
+                    veriler += xmloku.ReadString() + Environment.NewLine;
                 }
-
-
-
-
+                listBox1.Items.Add(veriler);
+                veriler = "";
             }
+        
+        }
+     
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            verileriyaz();
         }
     
      
